66f488938dbbed6913122a954d48c94b4ff68723
s4dmach1ne <just.reynaldo@gmail.com>
2021-12-04 12:00:41 +0700

    code review

--- app/src/main/java/com/rey/dobcalc/MainActivity.kt
+++ app/src/main/java/com/rey/dobcalc/MainActivity.kt
@@ -3,7 +3,6 @@ package com.rey.dobcalc
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import java.text.SimpleDateFormat
@@ -20,10 +19,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         val btnDatePicker : Button = findViewById(R.id.btnDatePicker)
         tvSelectedDate = findViewById(R.id.tvSelectedDate)
-
         tvAgeInSeconds = findViewById(R.id.tvAgeInSeconds)
         tvAgeInMinutes = findViewById(R.id.tvAgeInMinutes)
         tvAgeInHours = findViewById(R.id.tvAgeInHours)
@@ -33,39 +30,47 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
-    // private to make it not used by other class/file
+    // private to make it not used by other class/file (can only be used in main activity)
     private fun clickDatePicker(){
         val myCalendar = Calendar.getInstance()
-
         val year = myCalendar.get(Calendar.YEAR)
         val month = myCalendar.get(Calendar.MONTH)
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
 
-        val dpd = DatePickerDialog(this,R.style.datepicker,{ view, selectedYear, selectedMonth, selectedDayOfMonth ->
+        val dpd = DatePickerDialog(this,R.style.datepicker,{
+                view, selectedYear, selectedMonth, selectedDayOfMonth ->
+
                 val selectedDate = "$selectedDayOfMonth/${selectedMonth+1}/$selectedYear"
+
+                // tvSelectedDate?.setText(selectedDate)
                 tvSelectedDate?.text = selectedDate
 
+                // https://developer.android.com/reference/kotlin/java/text/SimpleDateFormat
                 val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
 
+                // converts selectedDate into theDate format
                 val theDate = sdf.parse(selectedDate)
+
                 // if date is not null, run the code (null safety)
                 theDate?.let{
 
                     // date to seconds,minutes,hours
-                    // time properties passed ever since 1 January 1970 00.00am in milliseconds
+                    // theDate.time properties passed ever since 1 January 1970 00.00am in milliseconds
+                    // milliseconds to proper seconds, minutes, and hours
                     val selectedDateInSeconds = theDate.time / 1000
                     val selectedDateInMinutes = theDate.time / 60000
                     val selectedDateInHours = theDate.time / 3600000
 
-                    // getting current dates
+                    // System.currentTimeMillis() getting current dates
                     // time properties passed ever since 1 January 1970 00.00am in milliseconds
                     val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
 
-
                     // if current date is not null, run the code (null safety)
                     currentDate?.let{
+
                         // date to seconds,minutes,hours
                         // time properties passed ever since 1 January 1970 00.00am in milliseconds
+                        // milliseconds to proper seconds, minutes, and hours
                         val currentDateInSeconds = currentDate.time / 1000
                         val currentDateInMinutes = currentDate.time / 60000
                         val currentDateInHours = currentDate.time / 3600000
@@ -83,7 +88,7 @@ class MainActivity : AppCompatActivity() {
                 }
             },year,month,day
         )
-        // Makes date picker cannot select future dates
+        // Makes date picker only select yesterday dates        24hrs
         dpd.datePicker.maxDate = System.currentTimeMillis() - 86400000
         dpd.show()
     }
